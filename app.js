be421a2db3c4680915b68835ef6a9ab2fc2dc7ab
safeer-farooq <sa.farooq.2k6@gmail.com>
2024-03-14 09:42:15 +0500

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+let num1 = prompt("ENTER YUR  1st  N.o")
+let num2 = prompt("ENTER YUR 2nd N.o")
+let result = (num1/num2)
+alert(result)
+
+let num3 = prompt("ENTER YUR  1st  N.o")
+let num4 = prompt("ENTER YUR 2nd N.o")
+let result1 = (num3*num4)
+alert(result1)
+
+let num5 = Number (prompt("ENTER YUR  1st  N.o"))
+let num6 = Number (prompt("ENTER YUR 2nd N.o"))
+let result2 = num5+num6
+alert(result2)
+
+let num7 = prompt("ENTER YUR  1st  N.o")
+let num8 = prompt("ENTER YUR 2nd N.o")
+let result3 = (num7-num8)
+alert(result3)
